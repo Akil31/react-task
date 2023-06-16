cd83b523d74f9317559cb14412929e56db5fa825
Akil31 <akilakil3108@gmail.com>
2023-06-16 23:55:58 +0530

    Co-authored-by: Bharathikva <Bharathikva@users.noreply.github.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 export default function (app) {
   console.log("Setup proxy is ever called");
   app.use(
-    '/api',
+    '/api/*',
     createProxyMiddleware({
       target: 'https://ahd.instapract.ae/web', // Replace with your backend server URL
       "secure": false,
